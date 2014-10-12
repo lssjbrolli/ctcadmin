a4937f441fe99ed067f5cb4a79f8f78d0d516577
Sergiu Cozma <lssjbrolli@gmail.com>
2014-10-12 21:27:32 +0300

    add phone, ferry, bridge expense types

--- app/models/extern_expense.rb
+++ app/models/extern_expense.rb
@@ -15,7 +15,7 @@ class ExternExpense < ActiveRecord::Base
   belongs_to :supplier, :foreign_key => 'supplier_id', :class_name => 'Company'
 
   CURRENCY    = %w(EUR RON HUF PLN DKK SEK NOK GBP)
-  DESCRIPTION = %w(Rent Loan Fuel Cash Service Parts)
+  DESCRIPTION = %w(Rent Loan Fuel Cash Service Parts Phone Ferry Bridge)
 
   def self.search(search)
     if search && !search.empty?
