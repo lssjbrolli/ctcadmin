b4aa4357ad6563236e13a714e4268b51d86414a7
Sergiu Cozma <lssjbrolli@gmail.com>
2014-11-05 13:08:22 +0200

    fix for papers db error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104200440) do
+ActiveRecord::Schema.define(version: 20141105110455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,10 +114,10 @@ ActiveRecord::Schema.define(version: 20141104200440) do
   create_table "papers", force: true do |t|
     t.string   "description"
     t.date     "expire"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "document_id"
     t.string   "document_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "comments"
   end
 
