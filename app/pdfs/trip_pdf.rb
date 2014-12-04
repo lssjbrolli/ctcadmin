d6e5cbd9619a0cef96b1a850b4ce974b05287a1d
Sergiu Cozma <lssjbrolli@gmail.com>
2014-12-04 16:03:51 +0200

    add card info and date styling to trip pdfs

--- app/pdfs/trip_pdf.rb
+++ app/pdfs/trip_pdf.rb
@@ -19,7 +19,7 @@ class TripPdf < Prawn::Document
 	end
 
 	def head
-		table = make_table([["Decont plati deplasare #{@dates[0].month}.#{@dates[0].year} - #{@dates[-1].month}.#{@dates[-1].year}"]], width: 523)
+		table = make_table([[ title ]], width: 540, column_widths: [540])
 		table.cells.style(size: 14, font_style: :bold, align: :center)
 
 		table.before_rendering_page do |page|
@@ -33,16 +33,16 @@ class TripPdf < Prawn::Document
 
 
 	def header
-		data  = ['Nr. crt.', 'Nr. actului', 'Data', 'Descrierea', 'Suma originala', 'Suma in EUR']
-		table = make_table([data], header: true, width: 523, column_widths: [48, 100, 75, 115, 100, 85])
+		data  = ['Nr. crt.', 'Nr. actului', 'Data', 'Descrierea', 'Card', 'Suma originala', 'Suma in EUR']
+		table = make_table([data], header: true, width: 540, column_widths: [45, 95, 60, 110, 40, 100, 90])
 
 		table.cells.style(size: 12, font_style: :bold)
-		#table.cells[0,0].style(width: 65)
 		table.cells[0, 1].style(align: :center)
 		table.cells[0, 2].style(align: :center)
 		table.cells[0, 3].style(align: :center)
-		table.cells[0, 4].style(align: :right)
+		table.cells[0, 4].style(align: :center)
 		table.cells[0, 5].style(align: :right)
+		table.cells[0, 6].style(align: :right)
 
 		table.before_rendering_page do |page|
 			page.row(0).border_top_width       = 2
@@ -55,16 +55,16 @@ class TripPdf < Prawn::Document
 
 	def line_items
 		@trip_expenses.map do |item|
-			["#{item.int_id}", "#{item.number}", "#{item.date}", "#{item.description}", @view.number_to_currency(item.value, :unit => item.currency), @view.number_to_currency(item.value_eur, unit: 'EUR')]
+			["#{item.int_id}", "#{item.number}", item.date.strftime("%d.%m.%y"), "#{item.description}", card(item), @view.number_to_currency(item.value, :unit => item.currency), @view.number_to_currency(item.value_eur, unit: 'EUR')]
 		end
 	end
 
 	def content
-		table = make_table line_items, width: 523, column_widths: [48, 100, 75, 115, 100, 85]
+		table = make_table line_items, width: 540, column_widths: [45, 95, 60, 110, 40, 100, 90]
 
 		table.cells.style(size: 10)
-		table.columns(0..3).style(align: :center)
-		table.columns(4..5).style(align: :right)
+		table.columns(0..4).style(align: :center)
+		table.columns(5..6).style(align: :right)
 
 
 		table.before_rendering_page do |page|
@@ -82,7 +82,7 @@ class TripPdf < Prawn::Document
 		@trip_expenses.each { |x| value_a << x.value_eur }
 		total = value_a.inject(0) { |r, e| r + e }
 
-		table = make_table [['', 'Total:', @view.number_to_currency(total, unit: 'EUR')]], width: 523, column_widths: [338, 100, 85]
+		table = make_table [['', 'Total:', @view.number_to_currency(total, unit: 'EUR')]], width: 540, column_widths: [350, 100, 90]
 
 		table.cells.style(size: 12, font_style: :bold)
 		table.cells[0, 1].style(align: :center)
@@ -96,5 +96,23 @@ class TripPdf < Prawn::Document
 		end
 
 		table.draw
+	end
+
+	private
+
+	def title
+		if @dates[0].month == @dates[-1].month
+			"Decont plati deplasare #{@dates[0].strftime("%B %Y")}"
+		else
+			"Decont plati deplasare #{@dates[0].strftime("%B %Y")} - #{@dates[-1].strftime("%B %Y")}"
+		end
+	end
+
+	def card(item)
+		if item.card
+			"Da"
+		else
+			"Nu"
+		end
 	end
 end
