a8e9c56951b80f2b8faf6fcedae0ae9d26996c43
Sergiu Cozma <lssjbrolli@gmail.com>
2015-04-12 19:29:03 +0300

    fix migration

--- db/migrate/20141128151847_rename_raport_to_report.rb
+++
@@ -1,5 +0,0 @@
-class RenameRaportToReport < ActiveRecord::Migration
-  def change
-    rename_table :raports, :reports
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202175245) do
+ActiveRecord::Schema.define(version: 20150412161428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,11 @@ ActiveRecord::Schema.define(version: 20141202175245) do
   end
 
   add_index "credit_notes", ["number"], name: "index_credit_notes_on_number", unique: true, using: :btree
+
+  create_table "employees", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.date     "date"
