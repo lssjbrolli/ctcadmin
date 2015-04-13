e83326f2e889842cc8f5c0260def81f115b62bf3
Sergiu Cozma <lssjbrolli@gmail.com>
2015-04-13 10:53:54 +0300

    employee has papers basic validates

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ActiveRecord::Base
+	validates :first_name, :last_name, :cnp, presence: true
+
+	has_many :papers, :as => :document, dependent: :destroy
+	belongs_to :created_by, :foreign_key => 'create_id', :class_name => 'User'
+	belongs_to :updated_by, :foreign_key => 'update_id', :class_name => 'User'
+
+	accepts_nested_attributes_for :papers, allow_destroy: true
 end
