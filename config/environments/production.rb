7aead04d83573cd1c5aa54f8f2044de8214860c0
Sergiu Cozma <lssjbrolli@gmail.com>
2015-12-28 23:56:10 +0200

    fix a deprecation warning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Ctcadmin::Application.configure do
 	# config.action_dispatch.rack_cache = true
 
 	# Disable Rails's static asset server (Apache or nginx will already do this).
-	#config.serve_static_assets = false
+	#config.serve_static_files = false
 
 	# Compress JavaScripts and CSS.
 	config.assets.js_compressor              = :uglifier
@@ -79,7 +79,7 @@ Ctcadmin::Application.configure do
 	config.log_formatter                     = ::Logger::Formatter.new
 
 	#heroku style fix http://stackoverflow.com/questions/16271696/cant-get-css-working-on-heroku-using-rails-4-with-bootstrap-saas-gem
-	config.serve_static_assets               = true
-	config.assets.compile                    = true
+	#config.serve_static_assets               = true
+	#config.assets.compile                    = true
 
 end
