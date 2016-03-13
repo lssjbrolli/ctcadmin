fa829455780712b7fe6e753fd0a6a38f43a3b75c
Sergiu Cozma <lssjbrolli@gmail.com>
2016-03-13 15:40:41 +0200

    fix tooltips text

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -3,9 +3,13 @@ module PapersHelper
 		today = Time.now.to_date
 		unless date.nil?
 			diff = (date - today).to_i
-			if diff <= 7
-				"class='error tip' data-title='This document has expired #{diff.abs} days ago.'"
-			elsif diff <= 30 && diff > 7
+			if diff == 0
+				"class='error tip' data-title='This document will expire today.'"
+			elsif diff < 0
+				"class='error tip' data-title='This document has expired #{pluralize(diff.abs, "day")} ago.'"
+			elsif diff > 0 && diff <= 7
+				"class='error tip' data-title='This document will expire in #{pluralize(diff.abs, "day")}.'"
+			elsif diff > 7 && diff < 30
 				"class='warning tip' data-title='This document will expire in #{diff} days.'"
 			end
 		end
