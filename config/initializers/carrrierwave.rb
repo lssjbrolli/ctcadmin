ffd60b1814d53ee055ccd4c994b65a909840a3d9
Sergiu Cozma <lssjbrolli@gmail.com>
2016-06-29 20:26:57 +0300

    tweaks

--- config/initializers/carrrierwave.rb
+++ config/initializers/carrrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   config.storage = :aws
-  config.aws_bucket = SiteConfig.find_by(var: 'util.s3_bucket').value.to_s
+  config.aws_bucket = SiteConfig.find_by(var: 'util.s3_bucket').value
   config.aws_acl = :public_read
   #config.asset_host = 'http://example.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
   config.aws_credentials = {
-      access_key_id: SiteConfig.find_by(var: 'util.s3_access_key_id').value.to_s,
-      secret_access_key: SiteConfig.find_by(var: 'util.s3_secret_access_key').value.to_s,
+      access_key_id: SiteConfig.find_by(var: 'util.s3_access_key_id').value,
+      secret_access_key: SiteConfig.find_by(var: 'util.s3_secret_access_key').value,
       region: 'eu-central-1',
       endpoint: 'http://s3.eu-central-1.amazonaws.com'
   }
