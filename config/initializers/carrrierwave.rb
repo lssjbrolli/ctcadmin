cfe74014ede21982c95701e3f70ffa727bc58d6f
Sergiu Cozma <lssjbrolli@gmail.com>
2016-07-22 18:20:04 +0300

    remove util settings

--- config/initializers/carrrierwave.rb
+++ config/initializers/carrrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   config.storage = :aws
-  config.aws_bucket = SiteConfig['util.s3_bucket']
+  config.aws_bucket = ENV['AWS_BUCKET']
   config.aws_acl = :public_read
   #config.asset_host = 'http://example.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
   config.aws_credentials = {
-      access_key_id: SiteConfig['util.s3_access_key_id'],
-      secret_access_key: SiteConfig['util.s3_secret_access_key'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'eu-central-1',
       endpoint: 'http://s3.eu-central-1.amazonaws.com'
   }
