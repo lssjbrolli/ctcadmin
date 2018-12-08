fac0f2a49e2b7a7edeb532df519844351fedf31b
Sergiu Cozma <lssjbrolli@gmail.com>
2018-12-08 21:27:17 +0200

    fix for countries gem

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,8 +1,12 @@
 module CompaniesHelper
   def eu_countries_list
-    full_list = ISO3166::Country.find_all_countries_by_eu_member(true)
+    full_list = ISO3166::Country.all
     list = []
-    full_list.each {|x| list.push(x.alpha2)}
+    full_list.each do |x|
+      if x.in_eu?
+        list.push(x.alpha2)
+      end
+    end
     list
   end
 end
