a0aac2e081f35fd248873ce4aca10afafb90fdf2
lssjbrolli <lssjbrolli@gmail.com>
2021-12-08 19:53:39 +0200

    don't show expired papers for sold vehicles

--- app/controllers/mainpage_controller.rb
+++ app/controllers/mainpage_controller.rb
@@ -7,6 +7,8 @@ class MainpageController < ApplicationController
   def index
     today = Time.now.to_date
     search_params = 'expire BETWEEN ? AND ? OR expire < ?'
-    @papers = Paper.where.not(expire: nil).where(search_params, today, (today + 30.day), today).order('expire asc', 'document_type desc')
+    @employees = Paper.where(document_type: "Employee")
+    @trucks = Paper.where(document_type: "Truck").where(document: Truck.where(sold: false))
+    @papers = @employees.or(@trucks).where.not(expire: nil).where(search_params, today, (today + 30.day), today).order('expire asc', 'document_type desc')
   end
 end
